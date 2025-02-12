ddc1042e9b3aab7dd437b033709e63b1d47e744b
victor <victorodrigues2312@gmail.com>
2025-02-12 11:13:49 -0300

    adicionando recurso de excluir filmes da Lista de Favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -10,9 +10,21 @@ function Favoritos() {
     setFilmes(JSON.parse(minhaLista) || []);
   }, []);
 
+  function excluirFilme(id) {
+    let filtroFilmes = filmes.filter((item) => {
+      return item.id !== id;
+    });
+
+    setFilmes(filtroFilmes);
+
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes));
+  }
+
   return (
     <div className="meus-filmes">
       <h1>Meus Filmes</h1>
+
+      {filmes.length === 0 && <span>Você não Possui Filmes na Lista :( </span>}
 
       <ul>
         {filmes.map((item) => {
@@ -22,7 +34,7 @@ function Favoritos() {
 
               <div>
                 <Link to={`/filme/${item.id}`}>Ver Detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
               </div>
             </li>
           );
